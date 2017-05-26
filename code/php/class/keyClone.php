aaeff495060f8dd105cfdfcead7bad9fef9917ff
libo <libo1@ethercap.com>
2017-05-26 13:44:01 +0800

    增加对象clone 测试语句

--- code/php/class/keyClone.php
+++ code/php/class/keyClone.php
@@ -9,16 +9,19 @@ class T1
 
     public function __construct()
     {
+        var_dump(__METHOD__);
         $this->__t2 = new T2();
     }
 
     public function get()
     {
+        var_dump(__METHOD__);
         return $this->__t2;
     }
 
     public function __clone()
     {
+        var_dump(__METHOD__);
         $this->__t2 = clone $this->__t2;
     }
 }
